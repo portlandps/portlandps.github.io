7aec706195f9bc5172d49f1b7bf7dc20de3da73d
C. David Maxey <cdmaxey@users.noreply.github.com>
2026-01-10 15:48:03 -0800

    Fix BreadcrumbList

--- _includes/post.js
+++ _includes/post.js
@@ -1,8 +1,8 @@
 <script type="application/ld+json">{% if page.tags.size > 1 %}[{% endif %}{% if page.tags %}{% for tag in page.tags %}{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[
-{"@type":"ListItem","position":1,"name":"{{site.data.x.schema_BreadcrumbList_Home}}","item":"{{site.url}}/"},
-{"@type":"ListItem","position":2,"name":"Articles","item":"{{site.url}}/articles/"},
-{"@type":"ListItem","position":3,"name":"{{tag}}","item":"{{site.url}}/articles/#{{tag|slugify}}"},
-{"@type":"ListItem","position":4,"name":"{{page.title_br[0]|remove:":"}}","item":"{{site.url}}{{page.url}}"}]}{% unless forloop.last %},{% endunless %}{% endfor %}{% if page.tags.size > 1 %}]{% endif %}{% else %}{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[
-{"@type":"ListItem","position":1,"name":"{{site.data.x.schema_BreadcrumbList_Home}}","item":"{{site.url}}/"},
-{"@type":"ListItem","position":2,"name":"Articles","item":"{{site.url}}/articles/"},
-{"@type":"ListItem","position":3,"name":"{{page.title_br[0]|remove:":"}}","item":"{{site.url}}{{page.url}}"}]}{% endif %}</script>
+{"@type":"ListItem","position":1,"item":{"@id":"{{site.url}}/","name":"{{site.data.x.schema_BreadcrumbList_Home}}"}},
+{"@type":"ListItem","position":2,"item":{"@id":"{{site.url}}/articles/","name":"Articles"}},
+{"@type":"ListItem","position":3,"item":{"@id":"{{site.url}}/articles/#{{tag|slugify}}","name":"{{tag}}"}},
+{"@type":"ListItem","position":4,"item":{"@id":"{{site.url}}{{page.url}}","name":"{{page.title_br[0]|remove:":"}}"}}]}{% unless forloop.last %},{% endunless %}{% endfor %}{% if page.tags.size > 1 %}]{% endif %}{% else %}{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[
+{"@type":"ListItem","position":1,"item":{"@id":"{{site.url}}/","name":"{{site.data.x.schema_BreadcrumbList_Home}}"}},
+{"@type":"ListItem","position":2,"item":{"@id":"{{site.url}}/articles/","name":"Articles"}},
+{"@type":"ListItem","position":3,"item":{"@id":"{{site.url}}{{page.url}}","name":"{{page.title_br[0]|remove:":"}}"}}]}{% endif %}</script>
