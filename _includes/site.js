55098e50ada3d502dc297bcc9cb8f956134a37d4
C. David Maxey <cdmaxey@users.noreply.github.com>
2026-02-16 14:48:23 -0800

    Update site.js

--- _includes/site.js
+++ _includes/site.js
@@ -1,3 +1,3 @@
 <script type="application/ld+json">{"@context":"https://schema.org","@type":"BreadcrumbList","itemListElement":[
 {"@type":"ListItem","position":1,"item":{"@id":"{{site.url}}/"}},
-{"@type":"ListItem","position":2,{% unless page.url == "/about-C-David-Maxey/" %}"name":"{{page.breadcrumb}}",{% endunless %}"item":{% if page.url == "/about-C-David-Maxey/" %}{"@id":"{{site.url}}{{page.url}}"}{% else %}"{{site.url}}{{page.url}}"{% endif %}}]}</script>
+{"@type":"ListItem","position":2,{% if page.url == "/about-C-David-Maxey/" %}"item":{"@id":"{{site.url}}{{page.url}}"}{% else %}"name":"{{page.breadcrumb}}","item":"{{site.url}}{{page.url}}"{% endif %}}]}</script>
