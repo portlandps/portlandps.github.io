d83ef5e9b0be4af22a4371a70309869410378881
C. David Maxey <cdmaxey@users.noreply.github.com>
2026-02-22 15:00:35 -0800

    Update schema.js

--- _includes/schema.js
+++ _includes/schema.js
@@ -1,4 +1,5 @@
-<script type="application/ld+json">{"@context":"https://schema.org","@graph":[{% if page.url == "/" %}{"@type":"WebSite","name":"{{site.data.x.company}}","url":"{{site.url}}{{page.url}}"},{% endif %}{% if page.layout == "post" %}{
+{% if page.url == "/" %}<script type="application/ld+json">{"@context":"https://schema.org","@type":"WebSite","name":"{{site.data.x.company}}","url":"{{site.url}}{{page.url}}"}</script>
+{% endif %}<script type="application/ld+json">{"@context":"https://schema.org","@graph":[{% if page.layout == "post" %}{
 "@type":"BlogPosting",
 "@id":"{{site.url}}{{page.url}}",
 "url":"{{site.url}}{{page.url}}",
