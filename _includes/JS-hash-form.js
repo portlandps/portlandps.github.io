9814b86e35f322b5373d0e19bd7c3511c8426adb
C. David Maxey <cdmaxey@users.noreply.github.com>
2026-04-28 22:44:46 -0700

    go

--- _includes/JS-hash-form.js
+++ _includes/JS-hash-form.js
@@ -1,33 +1,80 @@
-const inputOther=document.getElementById("inputother");
-function other(required){inputOther.disabled=!required;};
+const o=document.getElementById("o");
+function showother(required){o.disabled=!required;};
 const h=window.location.hash;
 if(h&&h.length>1){
+	if(h.startsWith("#?")){
+        const successSVG=document.getElementById("success");
+        if(successSVG){successSVG.style.display="block";}
+    }
 	const hP=new URLSearchParams(h.substring(1));
-	const links=document.querySelectorAll("a[data-p]");
-	links.forEach(link=>{
-		try{
-			let linkURL=new URL(link.href);
-			const includeAttr=link.getAttribute("data-p");
-			if(includeAttr){
-				const allowedKeys=includeAttr.split(",").map(key=>key.trim());
-				allowedKeys.forEach(key=>{if(hP.has(key)){linkURL.searchParams.set(key,hP.get(key));}});
-			} 
-			else{hP.forEach((value,key)=>{linkURL.searchParams.set(key,value);});}
-			link.href=linkURL.toString();}
-		catch(error){}
-	});
-	const keys=["f","l","b","i","a"];
-	keys.forEach(key=>{
+	const cuF=hP.get("customer_first_name");
+	const cuL=hP.get("customer_last_name");
+	const cuE=hP.get("customer_email");
+	const clF=hP.get("Client's_Legal_First_Name_(Child\u00A0or\u00A0Young\u00A0Adult)");
+	const clL=hP.get("Client's_Legal_Last_Name_(Child\u00A0or\u00A0Young\u00A0Adult)");
+	if(cuF!==null||cuL!==null||cuE!==null){
+		if(cuF===clF&&cuL===clL){
+			if(cuF){
+				const fInput=document.getElementById("f");
+				if(fInput){fInput.value=cuF;}}
+			if(cuL){
+				const lInput=document.getElementById("l");
+				if(lInput){lInput.value=cuL;}}
+			if(cuE){
+				const eInput=document.getElementById("e");
+				if(eInput){eInput.value=cuE;}}}
+		else{
+			if(clF){
+				const fInput=document.getElementById("f");
+				if(fInput){fInput.value=clF;}}
+			if(clL){
+				const lInput=document.getElementById("l");
+				if(lInput){lInput.value=clL;}}
+			if(cuF){
+				const pfInput=document.getElementById("pf");
+				if(pfInput){pfInput.value=cuF;}}
+			if(cuL){
+				const plInput=document.getElementById("pl");
+				if(plInput){plInput.value=cuL;}}
+            if(cuE){
+				const peInput=document.getElementById("pe");
+				if(peInput){peInput.value = cuE;}}}
+	}
+	const pMap={
+		"f":"f",
+		"l":"l",
+		"n":"n",
+		"b":"b",
+		"g":"g",
+		"e":"e",
+		"i":"i",
+		"o":"o",
+		"a":"a",	
+		"pf":"pf",
+		"pl":"pl",
+		"pn":"pn",
+		"pe":"pe",
+		"p":"parents",
+		"of":"of",
+		"ol":"ol",
+		"on":"on",
+		"oe":"oe"	
+	};
+	Object.keys(pMap).forEach(key=>{
 		let value=hP.get(key);
-		if(key==="i"){
-			if(value==="United"){document.getElementById("Radio_1").checked=true;}
-			else if(value==="other"){document.getElementById("Radio_others").checked=true;other(true);}
-			else if(value==="no"){document.getElementById("Radio_2").checked=true;}
+		const elementId = pMap[key];
+		if(elementId==="i"){
+			if(value==="United"){document.getElementById("United").checked=true;}
+			else if(value==="other"){document.getElementById("other").checked=true;showother(true);}
+			else if(value==="no"){document.getElementById("no").checked=true;}
+		}
+		else if(elementId==="g"&&["D","P","K","1","2","3","4","5","6","7","8","9","10","11","12","C","N"].includes(value)){
+			document.getElementById("g" + value).selected = true;
 		}
 		else{
-			const input=document.getElementById(key);
+			const input=document.getElementById(elementId);
 			if(value&&input){
-				if(key==="b"&&value.includes("-")){
+				if(elementId==="b"&&value.includes("-")){
 					const parts=value.split("-");
 					if(parts.length===3){
 						const[m,d,y]=parts;
@@ -42,9 +89,13 @@ if(h&&h.length>1){
 const today=new Date();
 today.setHours(0,0,0,0);
 const month=today.getMonth()+1;
+const year=today.getFullYear();
 const summer=document.getElementById("summer");
-if(month===5||month===6){summer.textContent="Select the grade in the past school year.";}
-	else if(month===7||month===8){summer.textContent="Select the grade for the upcoming school year.";}
+const gy=document.getElementById("gy");
+if(month<=6){gy.value=year;
+	if(month===5||month===6){summer.textContent="Select the grade in the past school year.";}}
+else{gy.value=year+1;
+	if(month===7||month===8){summer.textContent="Select the grade for the upcoming school year.";}}
 const b=document.getElementById("b");
 const eO=document.getElementById("eO");
 const e=document.getElementById("e");
@@ -88,14 +139,14 @@ function age(){
 	pe.required=!isAdult;
 	parentsO.hidden=isAdult;
 	parents.disabled=isAdult;
-	of.required=!isAdult&&parents.value==="1";
-	ol.required=!isAdult&&parents.value==="1";
-	oe.required=!isAdult&&parents.value==="1";
+	of.required=!isAdult&&parents.value==="another parent.";
+	ol.required=!isAdult&&parents.value==="another parent.";
+	oe.required=!isAdult&&parents.value==="another parent.";
 	parentOptional.textContent=isAdult?" (Optional)":"";
 	pay.textContent=isAdult?"Required if your parent is\u00A0paying\u00A0for\u00A0services.":"";
 	anotherOptional.textContent=isAdult?" (Optional)":"";
-	another.hidden=!isAdult&&["X","0"].includes(parents.value);
-	another.disabled=!isAdult&&["X","0"].includes(parents.value);
+	another.hidden=!isAdult&&["another parent who is now deceased.","never had another parent."].includes(parents.value);
+	another.disabled=!isAdult&&["another parent who is now deceased.","never had another parent."].includes(parents.value);
 }
 b.addEventListener("change",age);
 parents.addEventListener("change",age);
